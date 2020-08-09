3aa83cbce971f970bd31eb7243477d22948b1852
Andres <sewellandres@gmail.com>
2020-08-09 10:25:58 -0600

    [SupervisedLearner] Added doc comments to the two methods that didn't have them, adjusted variable names to be consistent with the rest of the toolkit

--- src/main/java/toolkit/SupervisedLearner.java
+++ src/main/java/toolkit/SupervisedLearner.java
@@ -6,10 +6,16 @@ package toolkit;
 
 public abstract class SupervisedLearner {
 
-	// Before you call this method, you need to divide your data
-	// into a feature matrix and a label matrix.
+	/**
+	 *
+	 * @param featuresOnlyDataMatrix a DataMatrix of values for the training algorithm to operate on
+	 * @param labelsOnlyDataMatrix a DataMatrix of labels corresponding to each row of features
+	 * @throws Exception
+	 *
+	 * Before you call this method, you need to divide your data
+	 * into a feature matrix and a label matrix.
+	 */
 	public abstract void train(DataMatrix featuresOnlyDataMatrix, DataMatrix labelsOnlyDataMatrix) throws Exception;
-
 
 	/**
 	 * 
@@ -17,7 +23,7 @@ public abstract class SupervisedLearner {
 	 * @param arrayInWhichToPutLabels array in which to put return values (i.e., label(s))
 	 * @throws Exception
 	 * 
-	 * 	A feature vector goes in. A label vector comes out. (Some supervised
+	 * A feature vector goes in. A label vector comes out. (Some supervised
 	 * learning algorithms only support one-dimensional label vectors. Some
 	 * support multi-dimensional label vectors.)
 	 * 
@@ -26,34 +32,43 @@ public abstract class SupervisedLearner {
 	 */
 	public abstract void predictInstanceLabelsFromFeatures(double[] featureVector, double[] arrayInWhichToPutLabels) throws Exception;
 
-	// The model must be trained before you call this method. If the label is nominal,
-	// it returns the predictive accuracy. If the label is continuous, it returns
-	// the root mean squared error (RMSE). If confusion is non-NULL, and the
-	// output label is nominal, then confusion will hold stats for a confusion matrix.
-	public double measurePredictiveAccuracy(DataMatrix features, DataMatrix labels, DataMatrix confusion) throws Exception
+	/**
+	 *
+	 * @param featuresOnlyDataMatrix a DataMatrix of values for the model to predict from
+	 * @param labelsOnlyDataMatrix a DataMatrix of labels corresponding to each row of features
+	 * @param confusion an empty DataMatrix that will be populated with the model's prediction data
+	 * @return a double representing the trained model's prediction accuracy
+	 * @throws Exception
+	 *
+	 * The model must be trained before you call this method. If the label is nominal,
+	 * it returns the predictive accuracy. If the label is continuous, it returns
+	 * the root mean squared error (RMSE). If confusion is non-NULL, and the
+	 * output label is nominal, then confusion will hold stats for a confusion matrix.
+	 */
+	public double measurePredictiveAccuracy(DataMatrix featuresOnlyDataMatrix, DataMatrix labelsOnlyDataMatrix, DataMatrix confusion) throws Exception
 	{
-		if(features.getRowCount() != labels.getRowCount())
-			throw(new Exception("Expected the features and labels to have the same number of rows"));
-		if(labels.getColCount() != 1)
-			throw(new Exception("Sorry, this method currently only supports one-dimensional labels"));
-		if(features.getRowCount() == 0)
-			throw(new Exception("Expected at least one row"));
+		if(featuresOnlyDataMatrix.getRowCount() != labelsOnlyDataMatrix.getRowCount())
+			throw new Exception("Expected the features and labels to have the same number of rows");
+		if(labelsOnlyDataMatrix.getColCount() != 1)
+			throw new Exception("Sorry, this method currently only supports one-dimensional labels");
+		if(featuresOnlyDataMatrix.getRowCount() == 0)
+			throw new Exception("Expected at least one row");
 
-		int labelValues = labels.getValueCountForAttributeAtColumn(0);
-		if(labelValues == 0) // If the label is continuous...
+		int labelValues = labelsOnlyDataMatrix.getValueCountForAttributeAtColumn(0);
+		if(labelValues == 0)
 		{
 			// The label is continuous, so measure root mean squared error
 			double sse = 0.0;
 			double[] predictedLabels = new double[1];
-			for(int i = 0; i < features.getRowCount(); i++)
+			for(int i = 0; i < featuresOnlyDataMatrix.getRowCount(); i++)
 			{
-				double[] featuresForInstance = features.getRow(i);
-				double[] targ = labels.getRow(i);
+				double[] featuresForInstance = featuresOnlyDataMatrix.getRow(i);
+				double[] target = labelsOnlyDataMatrix.getRow(i);
 				predictInstanceLabelsFromFeatures(featuresForInstance, predictedLabels);
-				double delta = targ[0] - predictedLabels[0];
+				double delta = target[0] - predictedLabels[0];
 				sse += (delta * delta);
 			}
-			return Math.sqrt(sse / features.getRowCount());
+			return Math.sqrt(sse / featuresOnlyDataMatrix.getRowCount());
 		}
 		else
 		{
@@ -62,25 +77,24 @@ public abstract class SupervisedLearner {
 			{
 				confusion.setSize(labelValues, labelValues);
 				for(int i = 0; i < labelValues; i++)
-					confusion.setAttributeName(i, labels.getAttributeValueName(0, i));
+					confusion.setAttributeName(i, labelsOnlyDataMatrix.getAttributeValueName(0, i));
 			}
 			int correctCount = 0;
-			for(int i = 0; i < features.getRowCount(); i++)
+			for(int i = 0; i < featuresOnlyDataMatrix.getRowCount(); i++)
 			{
-				double[] featuresForInstance = features.getRow(i);
-				int targ = (int)labels.getValueAt(i, 0);
-				if(targ >= labelValues)
+				double[] featuresForInstance = featuresOnlyDataMatrix.getRow(i);
+				int target = (int) labelsOnlyDataMatrix.getValueAt(i, 0);
+				if(target >= labelValues)
 					throw new Exception("The label is out of ransge");
 				double[] predictedLabels = new double[1]; 
 				predictInstanceLabelsFromFeatures(featuresForInstance, predictedLabels);
-				int pred = (int)predictedLabels[0];
+				int prediction = (int)predictedLabels[0];
 				if(confusion != null)
-					confusion.setValue(targ, pred, confusion.getValueAt(targ, pred) + 1);
-				if(pred == targ)
+					confusion.setValue(target, prediction, confusion.getValueAt(target, prediction) + 1);
+				if(prediction == target)
 					correctCount++;
 			}
-			return (double)correctCount / features.getRowCount();
+			return (double)correctCount / featuresOnlyDataMatrix.getRowCount();
 		}
 	}
-
 }
